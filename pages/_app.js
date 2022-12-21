026b6524c83524dbdb98f8e976b1aae64175d314
unknown <seanlattimoreii@gmail.com>
2022-12-21 03:04:29 -0500

    remove two .js files, add several .js components and pages, finally, change index.js and /_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '../styles/globals.css'
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes'; // External imports
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { Navbar, Footer } from '../components'; // Internal imports
+
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script src="https://kit.fontawesome.com/974feef76a.js" crossOrigin="anonymous" />
+  </ThemeProvider>
+);
+
+export default MyApp;
